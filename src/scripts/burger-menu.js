88668525de87dc7e6a414f400d8e83b28b2ce5f6
daifoll <kobykhnov.bl@gmail.com>
2022-03-12 21:03:37 +0700

    Написал скрипт бургер-меню

--- src/scripts/burger-menu.js
+++ src/scripts/burger-menu.js
@@ -2,20 +2,42 @@
 "use strict"
 
 const navigation = document.querySelector('.navigation');
+const navigationList = document.querySelector('.navigation__list');
 const navigationBtn = document.querySelector('.nav-btn');
-
-console.log(navigation);
+const navigationBtnItem = document.querySelector('.nav-btn__item');
 
 // События
 navigationBtn.addEventListener('click', openMenu);
+navigation.onload = navigationLoad();
+
 
 // Функции
-
 function openMenu(event){
     navigation.classList.toggle('navigation-active');
+    navigationBtnItem.classList.toggle('nav-btn__active');
 }
 
+function navigationLoad(event){
+    const headerNav = document.querySelector('.nav-header__list');
 
-if (document.documentElement.clientWidth > 768) {
+    for(let li of headerNav.children){
+        const aMain = li.children[0];
+
+        let liElement = document.createElement('li');
+        liElement.classList.add('list-nav__item')
+        navigationList.append(liElement);
+
+        let aElement = document.createElement('a');
+        aElement.classList.add(aMain.className);
+        aElement.href = aMain.href;
+        aElement.target = aMain.target;
+        aElement.textContent = li.textContent;
+        liElement.append(aElement);
+        
+        // console.log(aMain.className);
+    }
+    
+    
 
 }
+
