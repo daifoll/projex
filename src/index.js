14a168043c30e11f62abaff2fabde20205d1f261
daifoll <kobykhnov.bl@gmail.com>
2022-03-21 21:48:18 +0700

    Initial dist subtree commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './scripts/clients-slider.js'
-import './scripts/totop-button.js'
-import './scripts/provide-slider.js'
-import './scripts/connection-spoiler.js'
-import './scripts/burger-menu.js'
-import './styles/index.scss'
-import './index.pug'
