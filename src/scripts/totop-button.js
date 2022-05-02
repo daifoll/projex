3f3cc1ce83cf76190da04c80f465dbdf5cd322e9
daifoll <kobykhnov.bl@gmail.com>
2022-05-02 11:13:32 +0700

    Переписал скрипт кнопки наверх используя Observer

--- src/scripts/totop-button.js
+++ src/scripts/totop-button.js
@@ -1,35 +1,32 @@
 /* СКРИПТ КНОПКИ НА ВЕРХ */
+'use strict'
 
-let scrollStep = 4047;
-let btn = document.querySelector('.to-top')
-
-// События
-window.addEventListener('scroll', createToTopButton);
+// Константы
+const footer = document.querySelector('.footer')
 
 // Функции
-function createToTopButton(event){
-    let scrollCurrent = window.pageYOffset; 
-    let btn = document.querySelector('.to-top')
-    
-    if(btn){
-        if(scrollCurrent <= scrollStep){
-            btn.remove()
-        }else{
-            return false
-        }
-    }
-    
-
-    if(scrollCurrent >= scrollStep){
-        let btn = document.createElement('button');
-        btn.classList.add('to-top');
-        document.body.append(btn);
-
-        btn.addEventListener('click', toTopMove)
-    }  
-}
-
 function toTopMove(event){
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
+// Настриваиваем Intersection Observer API при пересечении секции footer
+let observer = new IntersectionObserver((entries, observer)=>{
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            let btn = document.createElement('button');
+            btn.classList.add('to-top');
+            document.body.append(btn);
+
+            btn.addEventListener('click', toTopMove)
+            
+        }else{
+            let btn = document.querySelector('.to-top')
+            if(btn){
+                btn.remove()
+            }
+        }
+    })
+}, {threshold: 0.5});
+
+// Инициализируем observer
+observer.observe(footer)
