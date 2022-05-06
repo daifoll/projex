326480f28ec97d0ed941c52204a6cebf2a703665
daifoll <kobykhnov.bl@gmail.com>
2022-05-06 23:11:06 +0700

    переписал аккордион и лобавил плавное открытие/закрытие

--- src/scripts/connection-spoiler.js
+++ src/scripts/connection-spoiler.js
@@ -2,24 +2,55 @@
 'use strict'
 
 const accordion = document.querySelector('.accordion-promises');
+const accordionItems = document.querySelectorAll('.item-accordion');
 
-// События
-accordion.addEventListener('click', openAccordion);
+for(let i = 0; i < accordionItems.length; i++){
+    accordionItems[i].addEventListener('click', openAccodrion)
+}
 
 
-// Функции
-function openAccordion(event){
-    let element = event.target;
-    let elementParent = element.parentNode;
-    let elementParentChildren = elementParent.children;
+
+function openAccodrion(){
+    if(this.classList.contains('item-accordion__content')) return
     
-    if(element.classList.contains('item-accordion__btn')){
-        for(let item of elementParentChildren){
-            item.classList.contains('item-accordion__text') ? item.classList.toggle('item-accordion__text_active') : item
-            item.classList.contains('item-accordion__btn') ? item.classList.toggle('item-accordion__btn_active') : item
-            element.textContent == '+' ? element.textContent = '-' : element.textContent = '+';
-            
-        }
-    }
+ 
+    this.classList.toggle('accordion-active');
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // События
+// accordion.addEventListener('click', openAccordion);
+
+
+
+// // Функции
+// function openAccordion(event){
+//     let element = event.target;
+//     let elementParent = element.parentNode;
+//     let elementParentChildren = elementParent.children;
+    
+//     if(element.classList.contains('item-accordion__btn')){
+//         for(let item of elementParentChildren){
+//             item.classList.contains('item-accordion__text') ? item.classList.toggle('item-accordion__text_active') : item
+//             item.classList.contains('item-accordion__btn') ? item.classList.toggle('item-accordion__btn_active') : item
+//             element.textContent == '+' ? element.textContent = '-' : element.textContent = '+';
+            
+//         }
+//     }
+
+// }
