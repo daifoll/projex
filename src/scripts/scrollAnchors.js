4ba1381a14884a0c88b50355c7d41d93e77b22c8
daifoll <kobykhnov.bl@gmail.com>
2022-05-29 12:51:41 +0700

    Добавил favicon, исправил баг скролла

--- src/scripts/scrollAnchors.js
+++ src/scripts/scrollAnchors.js
@@ -3,12 +3,11 @@
 // Импортируем библиотеку "ScrollToSmooth"
 import { ScrollToSmooth } from './scrolltosmooth.esm';
 
-
 // Настриваем бибилиотеку
 let smoothScroll = new ScrollToSmooth('a', {
     // options here
     duration: 400,
-    offset: document.querySelector('.header')
+    offset: document.getElementById('header'),
 
 });
 
