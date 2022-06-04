cf4ed2ca41daaa017458511771877b0cc2a351fb
daifoll <kobykhnov.bl@gmail.com>
2022-06-04 13:59:06 +0700

    Подключил библиотеку слайдера для promo, исправил неточночти в адаптивности

--- src/scripts/provide-slider.js
+++ src/scripts/provide-slider.js
@@ -1,66 +1,74 @@
-/* СКРИПТ СЛАЙДЕРА БЛОКА PROVIDE */
-"use strict"
-const slider = document.querySelector('.provide-slider')
-const sliderBtn = document.querySelector('.provide-btn');
-const sliderLine = document.querySelector('.provide-slider__line');
-const sliderBlock = document.querySelectorAll('.slider-provide');
-let sliderBlockWidth;
-let offSet = 0;
-let offWidth = 0;
-let sliderCount = 0;
-let sliderLeftStep = 0;
+// /* СКРИПТ СЛАЙДЕРА БЛОКА PROVIDE */
+// "use strict"
+// const slider = document.querySelector('.provide-slider')
+// const sliderBtn = document.querySelector('.provide-btn');
+// const sliderLine = document.querySelector('.provide-slider__line');
+// const sliderBlock = document.querySelectorAll('.slider-provide');
+// let sliderBlockWidth;
+// let offSet = 0;
+// let offWidth = 0;
+// let sliderCount = 0;
+// let sliderLeftStep = 0;
 
-// Задаём начальную ширину слайдера
-slider.style.width = 896 + 'px'
+// // Задаём начальную ширину слайдера
+// slider.style.width = 896 + 'px'
 
-// Определение ширины линии слайдера, в зависимости от количества элементов (при загрузке страницы)
-document.addEventListener("DOMContentLoaded", ()=>{
-    sliderBlockWidth = slider.clientWidth
-    sliderLine.style.width = sliderBlockWidth * sliderBlock.length + 'px';
-    offWidth = sliderLine.style.width;
-});
-
-
-
-// Определение ширины линии слайдера, в зависимости от количества элементов (при изменении ширины экрана)
-window.addEventListener('resize', ()=>{
-    sliderBlockWidth = slider.clientWidth
-    sliderLine.style.width = sliderBlockWidth * sliderBlock.length + 'px';
-    offWidth = sliderLine.style.width;
-
-    /* Устанавливаем значение left sliderLine при resize с сохранением положения текущего слайда */
-    if(sliderCount > 0){
-        sliderLine.style.left = -(Number(sliderLine.style.width.split('px')[0]) / sliderBlock.length) * sliderCount + 'px';
-    }else if(sliderCount == sliderBlock.length - 1){
-        sliderCount = 0;
-    }
-})
-
-// События
-sliderBtn.addEventListener('click', rightBtn);
+// // Определение ширины линии слайдера, в зависимости от количества элементов (при загрузке страницы)
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     sliderBlockWidth = slider.clientWidth
+//     sliderLine.style.width = sliderBlockWidth * sliderBlock.length + 'px';
+//     offWidth = sliderLine.style.width;
+// });
 
 
 
-// Функции
-function rightBtn(event){
-    offSet += -sliderBlockWidth;
-    if(offSet < -(parseInt(offWidth) - sliderBlockWidth)){
-        offSet = 0;
-    }
-    sliderLine.style.left = offSet + 'px';
+// // Определение ширины линии слайдера, в зависимости от количества элементов (при изменении ширины экрана)
+// window.addEventListener('resize', ()=>{
+//     sliderBlockWidth = slider.clientWidth
+//     sliderLine.style.width = sliderBlockWidth * sliderBlock.length + 'px';
+//     offWidth = sliderLine.style.width;
+
+//     /* Устанавливаем значение left sliderLine при resize с сохранением положения текущего слайда */
+//     if(sliderCount > 0){
+//         sliderLine.style.left = -(Number(sliderLine.style.width.split('px')[0]) / sliderBlock.length) * sliderCount + 'px';
+//     }else if(sliderCount == sliderBlock.length - 1){
+//         sliderCount = 0;
+//     }
+// })
+
+// // События
+// sliderBtn.addEventListener('click', rightBtn);
+
+
+
+// // Функции
+// function rightBtn(event){
+//     offSet += -sliderBlockWidth;
+//     if(offSet < -(parseInt(offWidth) - sliderBlockWidth)){
+//         offSet = 0;
+//     }
+//     sliderLine.style.left = offSet + 'px';
     
 
-    /*
-        Присваиваем переменной sliderCount значение текущего слайдера (отсчёт начинается с 0) 
-        для дальнейшей корректировки положения этого слайдера при resize
-    */
-    if(sliderCount == sliderBlock.length - 1){
-        sliderCount = 0;
-    }else{
-        sliderCount++
-    }
+//     /*
+//         Присваиваем переменной sliderCount значение текущего слайдера (отсчёт начинается с 0) 
+//         для дальнейшей корректировки положения этого слайдера при resize
+//     */
+//     if(sliderCount == sliderBlock.length - 1){
+//         sliderCount = 0;
+//     }else{
+//         sliderCount++
+//     }
 
-}
+// }
+// import { ItcSimpleSlider } from './simple-adaptive-slider.js'
 
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    // инициализация слайдера
+    new ItcSimpleSlider('.itcss', {
+      loop: true,
+      autoplay: false,
+      interval: 5000,
+      swipe: true,
+    });
+  });
